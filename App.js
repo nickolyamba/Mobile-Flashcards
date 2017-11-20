8d707dabf82938f853f45fd730f7e60532eb35a9
nickolyamba <nickolyamba@yahoo.com>
2017-11-19 21:14:35 -0800

    feat: Add Navigator to home screen

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { StackNavigator } from 'react-navigation'
+import DeckList from './components/DeckList';
+import Deck from './components/Deck';
 
-export default class App extends React.Component {
+
+const MainNavigator = StackNavigator({
+    Home: {
+        screen: DeckList,
+    },
+    Deck: {
+        screen: Deck,
+        navigationOptions: {
+            headerTintColor: white,
+            headerStyle: {
+                backgroundColor: purple,
+            }
+        }
+    }
+});
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>My Mobile App!</Text>
+          <MainNavigator />
       </View>
     );
   }
@@ -13,9 +34,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      paddingTop: 20
   },
 });
