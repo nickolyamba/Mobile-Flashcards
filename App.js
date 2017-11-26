7ce278c7c243963c4b2a8c1d0dd5ea8dd0d74f75
nickolyamba <nickolyamba@yahoo.com>
2017-11-25 22:36:22 -0800

    feat: Add TabNavigator as a main view

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
 import React, { Component }  from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { purple, white, grayOpacity } from './utils/colors'
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { purple, white, blue } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
-import { StackNavigator } from 'react-navigation'
+import { StackNavigator, TabNavigator } from 'react-navigation'
 import DeckList from './components/DeckList';
 import Deck from './components/Deck';
+import AddDeck from './components/AddDeck';
 
 
 const MainNavigator = StackNavigator({
     Home: {
         screen: DeckList,
         navigationOptions: {
-            title: 'Decks',
-            headerTintColor: purple,
-            headerStyle: {
-                backgroundColor: white,
-            }
-        }
+            header: null
+        },
     },
     Deck: {
         screen: Deck,
         navigationOptions: {
-            headerTintColor: purple,
+            headerTintColor: blue,
             headerStyle: {
                 backgroundColor: white,
             }
@@ -29,11 +26,55 @@ const MainNavigator = StackNavigator({
     }
 });
 
+const Tab = TabNavigator({
+        MainNavigator: {
+            screen: MainNavigator,
+            navigationOptions: {
+                tabBarLabel: 'Decks',
+                tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+            },
+        },
+        AddDeck: {
+            screen: AddDeck,
+            navigationOptions: {
+                tabBarLabel: 'New Deck',
+                tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+            },
+        },
+    },
+
+    // options source: UdaciFitness App and
+    // https://reactnavigation.org/docs/navigators/tab
+    {
+        navigationOptions: {
+            header: null
+        },
+        tabBarOptions: {
+            activeTintColor: Platform.OS === 'ios' ? blue : white,
+            style: {
+                height: 56,
+                backgroundColor: Platform.OS === 'ios' ? white : blue,
+                shadowColor: 'rgba(0, 0, 0, 0.24)',
+                shadowOffset: {
+                    width: 0,
+                    height: 3
+                },
+                shadowRadius: 6,
+                shadowOpacity: 1
+            },
+            labelStyle: {
+                fontSize: 20,
+                fontWeight: 'bold'
+            }
+        }
+    }
+);
+
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-          <MainNavigator />
+          <Tab />
       </View>
     );
   }
