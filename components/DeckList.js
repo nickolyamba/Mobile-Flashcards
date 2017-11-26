0c232ec3ed6e7ba48c7f6acb40be3945f6c6e695
nickolyamba <nickolyamba@yahoo.com>
2017-11-25 18:01:07 -0800

    feat: Populate state.decks from storage. Add DeckItem and header

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,19 +1,66 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 import { purple, white } from '../utils/colors'
+import { getDecks, getDeck, saveDeckTitle, addCardToDeck, removeCardFromDeck} from '../utils/storage';
+
+
+const DeckItem = ({deck, navigate}) => {
+  return(
+      <TouchableOpacity style={styles.card}
+                        onPress={() => navigate('Deck', { deck })}
+      >
+          <Text style={styles.buttonLargeText}>
+              {deck.title}
+          </Text>
+          <Text style={styles.buttonSmallText}>
+              {deck.questions ? deck.questions.length : ''} cards
+          </Text>
+      </TouchableOpacity>
+  )
+};
+
+renderHeader = () => {
+    return(
+        <Text style={{color: purple, fontSize: 25}}>
+            Decks
+        </Text>);
+};
 
 export default class DeckList extends Component {
+    state = {decks: []};
+
+    componentDidMount(){
+
+        // saveDeckTitle('JavaScript');
+        // addCardToDeck('Udacity', card).catch(err => console.error(err));
+        // removeCardFromDeck('Udacity', card).catch(err => console.error(err));
+
+        getDecks().then(decks => {
+            const deckArray = Object.keys(decks).reduce((deckArray, deckKey)=>{
+                if(!deckArray) deckArray = [];
+                deckArray.push(decks[deckKey]);
+                return deckArray;
+            }, []);
+
+            this.setState({decks: deckArray})
+        }).catch(err => console.log(err));
+
+
+    }
+
     render() {
         const {navigate} = this.props.navigation;
         return (
             <View style={styles.container}>
-                <TouchableOpacity style={styles.button}
-                                  onPress={() => navigate('Deck', { cardId: 'cardId #5' })}
-                >
-                    <Text style={styles.buttonText}>
-                        DeckList Go to Deck
-                    </Text>
-                </TouchableOpacity>
+                {this.state.decks &&
+                    <FlatList
+                        style={{flex: 1}}
+                        data={this.state.decks}
+                        renderItem={({item}) => <DeckItem deck={item} navigate={navigate}/> }
+                        keyExtractor={item => item.title}
+                        ListHeaderComponent={this.renderHeader}
+                    />
+                }
             </View>
         );
     }
@@ -23,19 +70,26 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: white,
-        alignItems: 'center',
-        justifyContent: 'center',
+        marginTop: 5
     },
-    buttonText: {
+    buttonLargeText: {
         color: white,
-        fontSize: 22,
+        fontWeight: 'bold',
+        fontSize: 30,
         textAlign: 'center',
     },
-    button: {
-        padding: 10,
-        backgroundColor: purple,
-        alignSelf: 'center',
-        borderRadius: 5,
-        margin: 20,
+    buttonSmallText: {
+        color: white,
+        fontSize: 20,
+        textAlign: 'center',
     },
+    card: {
+        flex: 1,
+        paddingVertical: 20,
+        backgroundColor: 'rgba(2, 179, 228, 1.0)',
+        borderRadius: 5,
+        marginBottom: 10,
+        marginLeft: 10,
+        marginRight: 10,
+    }
 });
