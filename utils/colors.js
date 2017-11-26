7c5e11c7955d32b11c4cb8d0dfcd2f6dbe779e2c
nickolyamba <nickolyamba@yahoo.com>
2017-11-25 22:33:19 -0800

    refactor: Update blue color code

--- utils/colors.js
+++ utils/colors.js
@@ -4,6 +4,6 @@ export const gray = '#757575';
 export const white = '#fff';
 export const red = '#b71845';
 export const orange = '#f26f28';
-export const blue = '#4e4cb8';
+export const blue = '#2196F3';
 export const lightPurp = '#7c53c3';
 export const pink = '#b93fb3';
